b5d9efa200f32d5c0603d35c438c5f270d1709b4
amsuhas1112 <amsuhas1112@gmail.com>
2026-03-15 17:19:29 +0530

    Initial Commit

---
+++ PexelVideoPlayer/PexelVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelVideoPlayerApp.swift
+//  PexelVideoPlayer
+//
+//  Created by Suhas A M on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PexelVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
